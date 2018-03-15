38e9640af4270abdaace025b8f1352cb0ae28d67
Michael Raitor <michael.raitor@gmail.com>
2018-03-15 13:22:29 +1100

    added plot funciton to display ankle pos relative to pelivs pos as viewed from the pelvis frame.

--- +grlib/+viz/UKF_Rel_LPVA_plot.m
+++ +grlib/+viz/UKF_Rel_LPVA_plot.m
@@ -1,5 +1,7 @@
 function UKF_Rel_LPVA_plot(N_MP,fs,actBody,estBody,pIntS)
 %close all
+plotWidth = 1000;
+plotHeight = 800;
 nDim = 3;
 % plotMP = 0;
 % plotLA = 3;
@@ -108,27 +110,30 @@ for k=1:3                              % plot results
   subplot(nDim,1,k)
   hold on
   plot((1:N_MP)/fs, pos_est_rel_MPFR(1:N_MP,k), '-', (1:N_MP)/fs, pos_act_rel_MPFR(1:N_MP,k), '--')
-  
-%   testPos = cumtrapz([1:N_MP]/fs,vel_act_rel_MPFR(k, 1:N_MP));
-%   plot((1:N_MP)/fs, testPos(1:N_MP))
-%   testPos = cumtrapz([1:N_MP]/fs,vel_rec_rel_MPFR(k, 1:N_MP));
-%   plot((1:N_MP)/fs, testPos(1:N_MP))
-  
-  
   ylabel(strcat('state ',num2str(k)))
   xlabel('time (s)')
   legend('est pos','tru pos','act. vel. int.','est. vel. int.')
+  set(gca,'fontsize',15)
+  box on
 end
-suptitle('rel. pos. in MP frame')
+suptitle(strcat('rel. pos. of',pIntS,'in MP frame'))
 hold off
+set(gcf, 'Position', [100, 100, plotWidth, plotHeight])
+set(gca,'fontsize',15)
+box on
+filename = strcat(pIntS,'Pos');
+formattype = 'png';
+saveas(gcf,filename,formattype)
 
 figure
 for k=1:3                              % plot results
   subplot(nDim+1,1,k)
   hold on
-  plot((1:N_MP)/fs,  pos_act_rel_MPFR(1:N_MP,k) - pos_est_rel_MPFR(1:N_MP,k), '--')
+  plot((1:N_MP)/fs,  pos_act_rel_MPFR(1:N_MP,k) - pos_est_rel_MPFR(1:N_MP,k))
   ylabel(strcat('state ',num2str(k)))
   xlabel('time (s)')
+  box on
+  set(gca,'fontsize',15)
   %legend('est pos','tru pos','act. vel. int.','est. vel. int.')
 end
 subplot(nDim+1,1,nDim+1)
@@ -136,12 +141,18 @@ ylabel('rms')
 pos_res = [pos_act_rel_MPFR(1:N_MP,:) - pos_est_rel_MPFR(1:N_MP,:)]';
 pos_res_rms = sqrt(sum(pos_res.^2,1));
 plot((1:N_MP)/fs,pos_res_rms)
-suptitle('pos residual (tru - est)')
+suptitle(strcat(pIntS,'pos residual (tru - est)'))
 hold off
+set(gcf, 'Position', [100, 100, plotWidth, plotHeight])
+set(gca,'fontsize',15)
+box on
+filename = strcat(pIntS,'Pos Res');
+formattype = 'png';
+saveas(gcf,filename,formattype)
 
 disp('rmse = ')
 disp(sum(pos_res_rms))
-% figure
+%% additional figure
 % plot((1:100)/100, cumtrapz([1:100]/100,[1:100]/100),'.')
 
 % figure
