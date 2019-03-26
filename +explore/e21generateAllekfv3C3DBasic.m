4f1912c43bfd9c1b67e5159685cf7e3fdfe2403e
Luke Sy <l.sy@unsw.edu.au>
2019-03-26 11:22:05 +1100

    generate analysis output

--- +explore/e21generateAllekfv3C3DBasic.m
+++ +explore/e21generateAllekfv3C3DBasic.m
@@ -4,11 +4,11 @@
 dir = 'neura-sparse01';
 expDir = sprintf('%s/explore-v2', dir);
 stepDir = sprintf('%s/step-detect', dir);
-outDir = sprintf('explore_output', dir);
-% outDir = 'C:\Users\z5151460\OneDrive - UNSW\Thesis - Sparse Mocap\Aim 1\Analysis - Step Fix - 01 Mar 2019\step-detect-v3-fixed-c3d';
+% outDir = sprintf('explore_output', dir);
+outDir = 'C:\Users\lukeyoga730\OneDrive - UNSW\Thesis - Sparse Mocap\Aim 1\Analysis - 20190312 - ckf results';
 ns = "NS2";
 % algo = "NS2+pfv1+Aw__sOw__sIw__v+Sav03+P001+M001";
-algo = "NS2+Aw__sOw__sIw__v+Sav03+M302+C000";
+algo = "";
 velThreshold = 0.1;
 
 dataList = readtable(sprintf('%s/data-list-v2.csv', dir));
@@ -18,75 +18,78 @@ options = struct('Pelvis', '00B40B91', ...
     'L_Foot', '00B40C55', 'R_Foot', '00B40C48');
 dataN = size(dataList, 1);
 
-for i = [1 5 7 15]
-    n = table2struct(dataList(i, :));
-    
-    name = sprintf("%s-%s", n.subj, n.act);
-    imuStepFName = sprintf("%s-imuStepDetect.csv", name);
-    revStepFName = sprintf("%s-revStepDetect.csv", name);
-    imuStep = readtable(sprintf("%s/%s", stepDir, imuStepFName));
-    viconStep = readtable(sprintf('%s/%s-viconStepDetect.csv', stepDir, name));
-    load(sprintf('%s/%s-%s-debug.mat', expDir, ns, name));
-    load(sprintf('%s/%s-%s-%s.mat', expDir, ns, name, algo));
-    revStepDetect = readtable(sprintf('%s/step-detect/%s-%s-revStepDetect.csv', ...
-                        dir, n.subj, n.act));
+for algoB = {"NS2+Aw__vOw__vIw__v+Sav03+M00+C355", "NS2+Aw__sOw__sIw__v+Sav03+M76+C355"}
+    algo = algoB{1};
+    for i = 1:dataN
+        n = table2struct(dataList(i, :));
 
-    targetname = sprintf('%s/%s-%s-%s', outDir, ns, name, algo);
-    
-    if cs.initSrc == 'w__v'
-        aLabel = 'w__v';
-        vb = W__viconBody;
-    elseif cs.initSrc == 'v__v'
-        aLabel = 'v__v';
-        vb = V__viconBody;
-    else
-        aLabel = 'w__x';
-        vb = W__xsensBody;
-    end
-    if ( strcmp(cs.accData, 'w__s') || strcmp(cs.accData, 'v__s') || ...
-       strcmp(cs.accData, 'w__sf') || strcmp(cs.accData, 'v__sf') )
-        eLabel = strcat(cs.accData, cs.initSrc(end));
-    else
-        eLabel = cs.accData;
-    end
-    idx = allIdx.(cs.initSrc);
-    
-    sensors = struct();
-    sensors.PELVFreeAcc = gfrAcc.(eLabel).MP;
-    sensors.LANKFreeAcc = gfrAcc.(eLabel).LA;
-    sensors.RANKFreeAcc = gfrAcc.(eLabel).RA;
-    sensors.PELVFreeAccRef = gfrAcc.(aLabel).MP;
-    sensors.LANKFreeAccRef = gfrAcc.(aLabel).LA;
-    sensors.RANKFreeAccRef = gfrAcc.(aLabel).RA;
-    sensors.PELVVel = estState(:, 4:6);
-    sensors.LANKVel = estState(:, 14:16);
-    sensors.RANKVel = estState(:, 24:26);
-    vel = vb.calcJointVel({'MIDPEL', 'LTIO', 'RTIO'});
-    sensors.PELVVelRef = vel.MIDPEL;
-    sensors.LANKVelRef = vel.LTIO;
-    sensors.RANKVelRef = vel.RTIO;
-    uwbdist = estBody.calcMPLARAdist();
-    sensors.MPLARAdist = [uwbdist.MPLA uwbdist.MPRA uwbdist.LARA];
-    uwbdistRef = vb.calcMPLARAdist();
-    sensors.MPLARAdistRef = [uwbdistRef.MPLA uwbdistRef.MPRA uwbdistRef.LARA];
-    
-    if strcmp(cs.stepDetection, 'av01')
-        bIsStatLA = imuStep.stepL(idx);
-        bIsStatRA = imuStep.stepR(idx);  
-    elseif strcmp(cs.stepDetection, 'av02')
-        bIsStatLA = viconStep.stepL(idx);
-        bIsStatRA = viconStep.stepR(idx);    
-    else % strcmp(cs.stepDetection, 'av03')
-        bIsStatLA = revStepDetect.stepL(idx);
-        bIsStatRA = revStepDetect.stepR(idx);
-    end
+        name = sprintf("%s-%s", n.subj, n.act);
+        imuStepFName = sprintf("%s-imuStepDetect.csv", name);
+        revStepFName = sprintf("%s-revStepDetect.csv", name);
+        imuStep = readtable(sprintf("%s/%s", stepDir, imuStepFName));
+        viconStep = readtable(sprintf('%s/%s-viconStepDetect.csv', stepDir, name));
+        load(sprintf('%s/%s-%s-debug.mat', expDir, ns, name));
+        load(sprintf('%s/%s-%s-%s.mat', expDir, ns, name, algo));
+        revStepDetect = readtable(sprintf('%s/step-detect/%s-%s-revStepDetect.csv', ...
+                            dir, n.subj, n.act));
 
-    nSamples = length(idx);
-    events = false(nSamples, 1);
-    
-    estBody.exportc3d(sprintf('%s.c3d', targetname), sensors, ...
-                      vb, bIsStatLA, bIsStatRA, struct(), 1);
-    fprintf("Data %3d/%3d: %s\n", i, dataN, name);
+        targetname = sprintf('%s/%s-%s-%s', outDir, ns, name, algo);
+
+        if cs.initSrc == 'w__v'
+            aLabel = 'w__v';
+            vb = W__viconBody;
+        elseif cs.initSrc == 'v__v'
+            aLabel = 'v__v';
+            vb = V__viconBody;
+        else
+            aLabel = 'w__x';
+            vb = W__xsensBody;
+        end
+        if ( strcmp(cs.accData, 'w__s') || strcmp(cs.accData, 'v__s') || ...
+           strcmp(cs.accData, 'w__sf') || strcmp(cs.accData, 'v__sf') )
+            eLabel = strcat(cs.accData, cs.initSrc(end));
+        else
+            eLabel = cs.accData;
+        end
+        idx = allIdx.(cs.initSrc);
+
+        sensors = struct();
+        sensors.PELVFreeAcc = gfrAcc.(eLabel).MP;
+        sensors.LANKFreeAcc = gfrAcc.(eLabel).LA;
+        sensors.RANKFreeAcc = gfrAcc.(eLabel).RA;
+        sensors.PELVFreeAccRef = gfrAcc.(aLabel).MP;
+        sensors.LANKFreeAccRef = gfrAcc.(aLabel).LA;
+        sensors.RANKFreeAccRef = gfrAcc.(aLabel).RA;
+        sensors.PELVVel = estState(:, 4:6);
+        sensors.LANKVel = estState(:, 14:16);
+        sensors.RANKVel = estState(:, 24:26);
+        vel = vb.calcJointVel({'MIDPEL', 'LTIO', 'RTIO'});
+        sensors.PELVVelRef = vel.MIDPEL;
+        sensors.LANKVelRef = vel.LTIO;
+        sensors.RANKVelRef = vel.RTIO;
+        uwbdist = estBody.calcMPLARAdist();
+        sensors.MPLARAdist = [uwbdist.MPLA uwbdist.MPRA uwbdist.LARA];
+        uwbdistRef = vb.calcMPLARAdist();
+        sensors.MPLARAdistRef = [uwbdistRef.MPLA uwbdistRef.MPRA uwbdistRef.LARA];
+
+        if strcmp(cs.stepDetection, 'av01')
+            bIsStatLA = imuStep.stepL(idx);
+            bIsStatRA = imuStep.stepR(idx);  
+        elseif strcmp(cs.stepDetection, 'av02')
+            bIsStatLA = viconStep.stepL(idx);
+            bIsStatRA = viconStep.stepR(idx);    
+        else % strcmp(cs.stepDetection, 'av03')
+            bIsStatLA = revStepDetect.stepL(idx);
+            bIsStatRA = revStepDetect.stepR(idx);
+        end
+
+        nSamples = length(idx);
+        events = false(nSamples, 1);
+
+        estBody.exportc3d(sprintf('%s.c3d', targetname), sensors, ...
+                          vb, bIsStatLA, bIsStatRA, struct(), 1);
+        fprintf("Data %3d/%3d: %s\n", i, dataN, name);
+    end
 end
 
 function [sIdx eIdx] = getStartEndIndices(events)
--- +pelib/+est/mpf_3_kmus_v1.m
+++ +pelib/+est/mpf_3_kmus_v1.m
@@ -104,7 +104,7 @@ function [ xhat_pri, xhat_pos, debug_dat ] = pf_3_kmus_v2(x0, P0, ...
     m.Al(idxPosMP,idxVelMP) = dt.*eye(3); % mid pelvis
     m.Al(idxPosLA,idxVelLA) = dt.*eye(3); % left ankle
     m.Al(idxPosRA,idxVelRA) = dt.*eye(3); % right ankle
-    m.Gn = eye(m.nxn, m.nxn);
+    m.Gn = eye(m.nxn,12); 
     m.Gl = zeros(m.nxl,9);
     m.Gl(idxPosMP, 1:3) = dt2.*eye(3);
     m.Gl(idxVelMP, 1:3) = dt .*eye(3);
@@ -135,18 +135,23 @@ function [ xhat_pri, xhat_pos, debug_dat ] = pf_3_kmus_v2(x0, P0, ...
     m.C(idxMMPtoRA, idxPosRA) =  eye(3, 3);
     m.C(idxMVelLA,idxVelLA) = eye(3,3); m.C(idxMPosZLA,idxPosLA(3)) = 1;
     m.C(idxMVelRA,idxVelRA) = eye(3,3); m.C(idxMPosZRA,idxPosRA(3)) = 1;
-    
+
+    m.dBody = struct('RPV', dPelvis, 'LTH', dLFemur, 'RTH', dRFemur, ...
+                     'LSK', dLTibia, 'RSK', dRTibia);
+    m.zFloor = mean([x0(idxPosLA(3)) x0(idxPosRA(3))]);
     debug_dat = {};
     
     % local variable assignment for readability
-    u_k = [gfrAccMP, gfrAccLA, gfrAccRA];
-    y_k = [qMP, qLA, qRA]';
+    ul_k = [gfrAccMP, gfrAccLA, gfrAccRA];
+    un_k = [qMP, qLA, qRA];
+    step = [bIsStatLA(:,1) bIsStatRA(:,1)];
+%     y_k = []';
     uwbMPLARA = [uwb_mea.left_tibia_mid_pelvis,...
                  uwb_mea.mid_pelvis_right_tibia,...
                  uwb_mea.left_tibia_right_tibia];
-    dBody = struct('RPV', dPelvis, 'LTH', dLFemur, 'RTH', dRFemur, ...
-                   'LSK', dLTibia, 'RSK', dRTibia);
-    zFloor = mean([body0.LTIO(1,3), body0.RTIO(1,3)]);
+
+    N = 1000;
+    g = mpf(m, ul_k, un_k, step, N)
     
     % allocate memory to store apriori and aposteriori state estimates, xhat,
     % and error covariances in the state estimate, P_pri, P_pos
@@ -161,150 +166,9 @@ function [ xhat_pri, xhat_pos, debug_dat ] = pf_3_kmus_v2(x0, P0, ...
     digit2ApplyMeas = mod(idivide(int32(applyMeas), 10, 'floor'), 10);
     modTenApplyMeas = mod(applyMeas, 10);
     
-    switch fOpt.applyPred
-        case 1
-            stateTransitionFcn = @stateTransitionFcn001;
-            Q = G * diag(sigmaQAccMP) * G' + diag([zeros(10, 1); deg2rad(1)*ones(8,1)]);
-        case 2
-            stateTransitionFcn = @stateTransitionFcn002;
-            sigma = struct('accMP', fOpt.sigmaQAccMP, ...
-                           'oriLK', deg2rad(0.5), 'oriRK', deg2rad(0.5));
-    end
-    
-    if digit2ApplyMeas == 0
-        measLikelihoodFcn = @measLikelihoodFcn001;
-%         R = diag([1e-2*ones(14,1); (fOpt.sigmaQAccLA)^2*ones(3,1); ...
-%               (fOpt.sigmaQAccRA)^2*ones(3,1)]);
-        R = diag([(fOpt.sigmaROriLSK).^2*ones(3,1); ...
-                  (fOpt.sigmaROriRSK).^2*ones(3,1);
-                  (fOpt.sigmaZuptLA).^2*ones(3,1); (fOpt.sigmaRPosLA).^2; ...
-                  (fOpt.sigmaZuptRA).^2*ones(3,1); (fOpt.sigmaRPosRA).^2; ...
-                  (fOpt.sigmaRAccLA).^2*ones(3,1)
-                  (fOpt.sigmaRAccRA).^2*ones(3,1)]);
-    elseif digit2ApplyMeas == 1
-        measLikelihoodFcn = @measLikelihoodFcn010;
-        R = diag([(fOpt.sigmaRVelLA).^2*ones(3,1); ...
-                  (fOpt.sigmaRVelRA).^2*ones(3,1);
-                  (fOpt.sigmaRPosLA).^2; (fOpt.sigmaRPosRA).^2; ...
-                  (fOpt.sigmaRVelMP).^2*ones(3,1)]);
-              
-        idxVelLA = 01:03; % column idx corresponding to the left ankle velocity
-        idxVelRA = 04:06; % column idx corresponding to the right ankle velocity
-        kfNStates = 6;
-        
-        F = eye(kfNStates, kfNStates);        
-        G = zeros(kfNStates,6);        
-        G(idxVelLA, 1:3) = dt .*eye(3);
-        G(idxVelRA, 4:6) = dt .*eye(3);
-        kfQ = G*diag(repelem([(fOpt.sigmaQAccLA)^2 (fOpt.sigmaQAccRA)^2], 3))*G';
-        u_k = [gfrAccLA, gfrAccRA]';
-        
-        idxMVelLA = 1:3;
-        idxMVelRA = 4:6;
-        kfNMeasure = 6;
-        
-        H = zeros(kfNMeasure, kfNStates);
-        H(idxMVelLA, idxVelLA) = eye(3);
-        H(idxMVelRA, idxVelRA) = eye(3);
-        Rdiag = repelem([(fOpt.sigmaZuptLA)^2 (fOpt.sigmaZuptRA)^2], 3);
-        kfR = diag(Rdiag);       
-        y_k = zeros(kfNMeasure, nSamples);
-        
-        kfI_N = eye(kfNStates,kfNStates);
-        kfxPri = zeros(kfNStates,1);
-        kfxPos = [vel0.LA vel0.RA]';
-        kfPPri = kfI_N;
-        kfPPos = 1*kfI_N;
-        
-        kfxListPri = nan(nSamples, kfNStates);
-        kfPListPri = nan(kfNStates, kfNStates, nSamples);
-        kfxListPos = nan(nSamples, kfNStates);
-        kfPListPos = nan(kfNStates, kfNStates, nSamples);
-    end
-    
-    pf = particleFilter(stateTransitionFcn, measLikelihoodFcn);
-    pf.StateEstimationMethod = 'mean';
-    pf.ResamplingMethod = 'systematic';
-    nParticles = 10000;
-    initialize(pf, nParticles, x0, P0);
-
-    for n = 1:nSamples
-        %% Predict next position. Resample particles if necessary.
-        switch fOpt.applyPred
-            case 1
-                [xhat_pri(n,:), P_pri(:,:,n)] = predict(pf, fs, Q, ...
-                                        gfrAccMP(n, :)', qMP(n, :)');
-            case 2
-                qOri = struct('qRPV', qMP(n, :), ...
-                              'qLSK', qLA(n, :), 'qRSK', qRA(n, :));
-                [xhat_pri(n,:), P_pri(:,:,n)] = predict(pf, fs, sigma, ...
-                                        gfrAccMP(n, :)', qOri);
-        end
-        % Generate dot measurement with random noise. This is
-        % equivalent to the observation step.
-%         measurement(i,:) = dot(i,:) + noise*(rand([1 2])-noise/2);
-        %% Correct position based on the given measurement to get best estimation.
-        % Actual dot position is not used. Store corrected position in data array.
-        if digit2ApplyMeas == 0
-            step = [bIsStatLA(n, 1) bIsStatRA(n, 1)];
-            if n-1 >= 1, past1Particles = xhat_pos(n-1,:)';
-            else, past1Particles = x0; end
-            if n-2 >= 1, past2Particles = xhat_pos(n-2,:)';
-            else, past2Particles = x0; end
-        
-            meas = [qLA(n, :) qRA(n, :) gfrAccLA(n, :) gfrAccRA(n,:)];
-            [xhat_pos(n,:), P_pos(:,:,n)] = correct(pf, meas, fs, R, applyMeas, ...
-                    dBody, step, zFloor, past1Particles, past2Particles);
-        elseif digit2ApplyMeas == 1
-            if n-1 >= 1, past1Particles = xhat_pos(n-1,:)';
-            else, past1Particles = x0; end
-            
-            kfxPri = F * kfxPos + G * u_k(:,n) ;
-            kfPPri = F * kfPPos * F' + kfQ;
-            
-            nIdx = 0;
-            if bIsStatLA(n, 1), nIdx = nIdx + 3; end
-            if bIsStatRA(n, 1), nIdx = nIdx + 3; end
-            idx = zeros(nIdx, 1); tmpIdxR = 1;
-            
-            if bIsStatLA(n, 1)
-                idx(tmpIdxR:tmpIdxR+2) = idxMVelLA;
-                tmpIdxR = tmpIdxR+3;
-            end
-            if bIsStatRA(n, 1)
-                idx(tmpIdxR:tmpIdxR+2) = idxMVelRA;
-                tmpIdxR = tmpIdxR+3;
-            end
-            
-            res = y_k(idx, n) - H(idx, :) * kfxPri;
-            K = kfPPri * H(idx, :)' /(H(idx, :) * kfPPri * H(idx,:)' + kfR(idx, idx));
-            kfxPos = kfxPri + K * res;
-            kfPPos = (kfI_N - K * H(idx, :)) * kfPPri;
-            
-            kfxListPri(n,:) = kfxPri;
-            kfPListPri(:,:,n) = kfPPri;
-            kfxListPos(n,:) = kfxPos;
-            kfPListPos(:,:,n) = kfPPri;
-        
-            meas = struct();
-            if bitand(modTenApplyMeas, 1)
-                meas.velLA = kfxPos(idxVelLA,:)';
-                meas.velRA = kfxPos(idxVelRA,:)';
-            end
-            if bitand(modTenApplyMeas, 2)
-                meas.posZLA = zFloor; meas.posZRA = zFloor;
-            end
-            if bitand(modTenApplyMeas, 4)
-                meas.velMP = 0;
-            end
-            
-            [xhat_pos(n,:), P_pos(:,:,n)] = correct(pf, meas, fs, R, ...
-                    applyMeas, dBody, past1Particles);
-        end
-    end
 end
 
-function g = mpf(y,m,u_k,step,N)
+function g = mpf(m,ul_k,un_k,step,N)
     % index list
     idxPosMP =  1: 3; idxVelMP =  4: 6; idxPosLA =  7: 9;
     idxVelLA = 10:12; idxPosRA = 13:15; idxVelRA = 16:18;
@@ -314,18 +178,21 @@ function g = mpf(y,m,u_k,step,N)
     idxMVelLA = 7:9; idxMPosZLA = 10;
     idxMVelRA = 11:13; idxMPosZRA = 14;
     
-    Tfinal = size(y,2);         % Number of samples (measurements)
+    Tfinal = size(ul_k,2);         % Number of samples (measurements)
     NrPart = N;                 % Number of particles
     xf     = zeros(m.nx, Tfinal);   % Allocate room for the filtered estimates
 
     % (1) Initialize
-    xnp = repmat(m.x0(m.nxl+1:end),1,NrPart) + ...
-        chol(m.P0(m.nxl+1:end,m.nxl+1:end))*randn(m.nxn,NrPart);  % Nonlinear states
-    xlp = repmat(m.x0(1:m.nxl),1,NrPart);      % Conditionally linear Gaussian states
-    Pl  = m.P0(1:m.nxl,1:m.nxl);
+    idxxl = 1:m.nxl;
+    idxxn = m.nxl+1:m.nx;
+    xnp = repmat(m.x0(idxxn),1,NrPart) + ...
+        chol(m.P0(idxxn,idxxn))*randn(m.nxn,NrPart);  % Nonlinear states
+    xlp = repmat(m.x0(idxxl),1,NrPart);      % Conditionally linear Gaussian states
+    Pl  = m.P0(idxxl,idxxl);
     Pp  = repmat(Pl,[1,1,NrPart]);             % Initial covariance matrix
     xlf = zeros(size(xlp));                    % Allocate room for the filtered quantities
     Pf  = zeros(size(Pp));                     % Allocate room for the filtered quantities
+    q   = zeros(1,NrPart);
     for t=1:Tfinal
         idxN = 8;
         if step(1,t), idxN = idxN+4; end % left  step detect
@@ -340,18 +207,26 @@ function g = mpf(y,m,u_k,step,N)
             idx(tmpiIdx:tmpiIdx+3) = [idxMVelRA idxMPosZRA];
             tmpiIdx = tmpiIdx + 4;
         end
-        yNow = [calcLinearHJCd(qRPV, qLSK, qRSK, alphaLK, alphaRK, dBody); ...
-            ];
-        yhat = m.C*xlp;
-        e    = repmat(yNow,1,NrPart) - yhat;
+        yNow = [calcLinearHJCd(xlp(idxOriMP,:), ...
+                               xlp(idxOriLA,:), xlp(idxOriRA,:), ...
+                               xlp(idxOriLK,:), xlp(idxOriRK,:), ...
+                               m.dBody); ...
+                zeros(3,NrPart); repelem(m.zFloor,1,NrPart); ...
+                zeros(3,NrPart); repelem(m.zFloor,1,NrPart) ];
+        C2 = m.C(idx,:); R2 = m.R(idx,idx);
+        Qn = m.Q(idxxn,idxxn);
+        Ql = m.Q(idxxl,idxxl);
+        yhat = C2*xlp;
+        e    = yNow(idx) - yhat;
         % (2) Compute the importance weights according to eq. (25a)
         for i=1:NrPart
-          M = m.C*Pp(:,:,i)*m.C' + m.R;
-          q(i) = exp(-(1/2)*(e(:,i)'*inv(M)*e(:,i)));
+          M = C2*Pp(:,:,i)*C2' + R2;
+          q(i) = exp(-(1/2)*(e(:,i)'/M*e(:,i)));
+%           q(i) = exp(-(1/2)*(e(:,i)'*inv(M)*e(:,i)));
         end
         if(sum(q)>1e-12)                % Divergence check
           q       = q/sum(q);           % Normalize the importance weights
-          xf(1,t) = sum(q.*xnp,2);      % Compute estimate for the nonlinear states
+          xf(idxxn,t) = sum(q.*xnp,2);      % Compute estimate for the nonlinear states
           index   = sysresample(q);     % (3) Resample
           xnp     = xnp(:,index);       % Resampled nonlinear particles
           xlp     = xlp(:,index);       % Resampled linear particles
@@ -361,32 +236,37 @@ function g = mpf(y,m,u_k,step,N)
           info    = 0;
         else   % The filter has diverged
           info = 1;
-          xf   = zeros(4,Tfinal);
+          xf   = zeros(m.n,Tfinal);
           disp(['Weights close to zero at t=',num2str(t),' !!!']);
           return;
-        end;
+        end
         % (4a) KF MU
         for i = 1:NrPart
-          M         = m.C*Pp(:,:,i)*m.C' + m.R;    % Eq. (22c)
-          K         = Pp(:,:,i)*m.C'*inv(M);       % Eq. (22d)
-          yhat      = [(0.1*xnp(:,i).^2).*sign(xnp(:,i)); xlp(1,i) - xlp(2,i) + xlp(3,i)];
-          xlf(:,i)  = xlp(:,i) + K*(yNow - yhat);  % Eq. (22a)
+          M         = C2*Pp(:,:,i)*C2' + R2;    % Eq. (22c)
+          K         = Pp(:,:,i)*C2'/M;       % Eq. (22d)
+          yhat      = C2*xlp;
+          yNow      = [calcLinearHJCd(xlp(idxOriMP,i), ...
+                               xlp(idxOriLA,i), xlp(idxOriRA,i), ...
+                               xlp(idxOriLK,i), xlp(idxOriRK,i), ...
+                               m.dBody); ...
+                       zeros(3,1); m.zFloor; zeros(3,1); m.zFloor ];
+          xlf(:,i)  = xlp(:,i) + K*(yNow(idx,1) - yhat);  % Eq. (22a)
           Pf(:,:,i) = Pp(:,:,i) - K*M*K';          % Eq. (22b)
-        end;
-        xf(2:4,t) = mean(xlf,2);    % Compute estimate for the linear states
+        end
+        xf(idxxl,t) = mean(xlf,2);    % Compute estimate for the linear states
         % (4b) PF prediction according to Eq. (25b)
         xnf = xnp;
         for i = 1:NrPart
-          xnp(i) = atan(xnf(i)) + xlf(1,i) + sqrt(m.An*Pf(:,:,i)*m.An' + m.Q(1,1))*randn(1);
-        end;
+          xnp(i) = m.An*xnf(i) + m.Gn*un_k(:,t) + Qn*randn(m.nxn,1);
+        end
         % (4c) KF TU
         for i = 1:NrPart
-          N         = m.An*Pf(:,:,i)*m.An' + m.Q(1,1);       % Eq. (23c)
-          L         = m.Al*Pf(:,:,i)*m.An'*inv(N);           % Eq. (23d)
-          z         = xnp(i) - atan(xnf(i));                 % Eq. (24a)
-          xlp(:,i)  = m.Al*xlf(:,i) + L*(z - m.An*xlf(:,i)); % Eq. (23a)
-          Pp(:,:,i) = m.Al*Pf(:,:,i)*m.Al' + m.Q(2:end,2:end) - L*N*L'; % Eq. (23b)
-        end;
+          N         = m.An*Pf(:,:,i)*m.An' + Qn;             % Eq. (23c)
+          L         = m.Al*Pf(:,:,i)*m.An'/N;                % Eq. (23d)
+          z         = xnp(i) - x.fn;                         % Eq. (24a)
+          xlp(:,i)  = m.Al*xlf(:,i) + m.Gl*ul_k(:,t) + L*(z - m.An*xlf(:,i)); % Eq. (23a)
+          Pp(:,:,i) = m.Al*Pf(:,:,i)*m.Al' + Ql - L*N*L'; % Eq. (23b)
+        end
     end
     g.xf   = xf;
     g.info = info;
