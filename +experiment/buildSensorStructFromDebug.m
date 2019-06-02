45ab4d210f00d8ab25715300665ab86dafb03e82
Luke Sy <l.sy@unsw.edu.au>
2019-06-02 18:18:01 +1000

    vel constraint at meas update (lieekf v1)

---
+++ +experiment/buildSensorStructFromDebug.m
@@ -0,0 +1,47 @@
+% ======================================================================
+%> @brief generate sensor struct debug versions
+%> @author Luke Sy (UNSW GSBME)
+%> @date 27 May 2019
+%>
+%>
+%> @param sensors struct containing sensor data
+%> @param state debug information
+%> @param algo estimator mode
+%>
+%> @retval out grBody
+% ======================================================================
+function sensors = buildSensorStructFromDebug(sensors, state, state2, algo, suffix)
+    if nargin <= 4
+        suffix = '';
+    end
+    
+    if strcmp(algo, 'lieekfv1')
+        idx = {{'PELV', 1:3}, {'LANK', 4:6}, {'RANK', 7:9}};
+        for i=1:3
+            sname = sprintf('%sVel%s', idx{i}{1}, suffix);
+            sensors.(sname) = state.vec(idx{i}{2}, :)';
+        end
+           
+        idx = {{'PV',  1: 3,  4: 6, 19:21, 28:30}, ...
+               {'LS',  7: 9, 10:12, 22:24, 31:33}, ...
+               {'RS', 13:15, 16:18, 25:27, 34:36}};
+        idx2 = {'Pos', 'Ori', 'Vel', 'AVel'};
+        [nState, ~, nSamples] = size(state2.Ptilde);
+        for i=1:3
+            buf = zeros(nSamples, nState);
+            for j=1:nSamples
+                buf(j,:) = diag(state2.Ptilde(:,:,j));
+            end
+            for j=1:length(idx2)
+            	sname = sprintf('P%s%s%s', idx2{j}, idx{i}{1}, suffix);
+            	sensors.(sname) = buf(:,idx{i}{j+1});
+            end
+        end
+    else
+        idx = {{'PELV',  4: 6}, {'LANK', 14:16}, {'RANK', 24:26}};
+        for i=1:3
+            sname = sprintf('%sVel%s', idx{i}{1}, suffix);
+            sensors.(sname) = state(:, idx{i}{2});
+        end
+    end
+end
---
+++ +experiment/buildgrBodyDebug.m
@@ -0,0 +1,123 @@
+% ======================================================================
+%> @brief generate grBody debug versions
+%> @author Luke Sy (UNSW GSBME)
+%> @date 27 May 2019
+%>
+%>
+%> @param body grBody
+%> @param sensors struct containing sensor data
+%> @param debugData debug information
+%> @param algo estimator mode
+%> @param suffix added to the field name appended to sensorsDebug struct
+%>
+%> @retval out grBody
+% ======================================================================
+function [bodyDebug, sensorsDebug] = buildgrBodyDebug(body, sensors, debugData, algo, suffix)
+    if nargin <= 4
+        suffix = '';
+    end    
+    bodyDebug = body.copy();
+    n2 = body.nSamples*3;
+    
+    if isstruct(sensors)
+        sensorsDebug = struct();
+        fn = fieldnames(sensors);
+        for i=1:length(fn)
+            n = fn{i};
+            sensorsDebug.(n) = repelem(sensors.(n), 3, 1);
+        end
+    else
+        sensorsDebug = struct();;
+    end
+    
+    if strcmp(algo, 'vicon')
+        bodyDebug = body.copy();
+        for i=1:length(bodyDebug.posList)
+            n = bodyDebug.posList{i};
+            bodyDebug.(n) = repelem(bodyDebug.(n), 3, 1);
+        end
+        for i=1:length(bodyDebug.oriList)
+            n = bodyDebug.oriList{i};
+            bodyDebug.(n) = repelem(bodyDebug.(n), 3, 1);
+        end
+    else
+        bodyDebug.MIDPEL = repelem(bodyDebug.MIDPEL, 3, 1);
+        bodyDebug.LTIO = repelem(bodyDebug.LTIO, 3, 1);
+        bodyDebug.RTIO = repelem(bodyDebug.RTIO, 3, 1);
+        bodyDebug.qRPV = repelem(bodyDebug.qRPV, 3, 1);
+        bodyDebug.qLSK = repelem(bodyDebug.qLSK, 3, 1);
+        bodyDebug.qRSK = repelem(bodyDebug.qRSK, 3, 1);
+
+        if strcmp(algo, 'lieekfv1')
+            bodyDebug.MIDPEL(1:3:n2, :) = squeeze(debugData.xhatPri.W_T_PV(1:3,4,:))';
+            bodyDebug.MIDPEL(2:3:n2, :) = squeeze(debugData.xhatPos.W_T_PV(1:3,4,:))';
+            bodyDebug.LTIO(1:3:n2, :) = squeeze(debugData.xhatPri.W_T_LS(1:3,4,:))';
+            bodyDebug.LTIO(2:3:n2, :) = squeeze(debugData.xhatPos.W_T_LS(1:3,4,:))';
+            bodyDebug.RTIO(1:3:n2, :) = squeeze(debugData.xhatPri.W_T_RS(1:3,4,:))';
+            bodyDebug.RTIO(2:3:n2, :) = squeeze(debugData.xhatPos.W_T_RS(1:3,4,:))';
+            bodyDebug.qRPV(1:3:n2, :) = rotm2quat(debugData.xhatPri.W_T_PV(1:3,1:3,:));
+            bodyDebug.qRPV(2:3:n2, :) = rotm2quat(debugData.xhatPos.W_T_PV(1:3,1:3,:));
+            bodyDebug.qLSK(1:3:n2, :) = rotm2quat(debugData.xhatPri.W_T_LS(1:3,1:3,:));
+            bodyDebug.qLSK(2:3:n2, :) = rotm2quat(debugData.xhatPos.W_T_LS(1:3,1:3,:));
+            bodyDebug.qRSK(1:3:n2, :) = rotm2quat(debugData.xhatPri.W_T_RS(1:3,1:3,:));
+            bodyDebug.qRSK(2:3:n2, :) = rotm2quat(debugData.xhatPos.W_T_RS(1:3,1:3,:));
+        else
+            bodyDebug.MIDPEL(1:3:n2, :) = debugData.predState(:,1:3);
+            bodyDebug.MIDPEL(2:3:n2, :) = debugData.zuptState(:,1:3);
+            bodyDebug.LTIO(1:3:n2, :) = debugData.predState(:,11:13);
+            bodyDebug.LTIO(2:3:n2, :) = debugData.zuptState(:,11:13);
+            bodyDebug.RTIO(1:3:n2, :) = debugData.predState(:,21:23);
+            bodyDebug.RTIO(2:3:n2, :) = debugData.zuptState(:,21:23);
+        end
+
+        v = quat2rotm(bodyDebug.qLSK); v = squeeze(v(:,3,:))';
+        bodyDebug.LFEO = bodyDebug.LTIO + bodyDebug.getLShankLength()*v;
+        v = quat2rotm(bodyDebug.qRSK); v = squeeze(v(:,3,:))';
+        bodyDebug.RFEO = bodyDebug.RTIO + bodyDebug.getRShankLength()*v;
+        v = quat2rotm(bodyDebug.qRPV); v = squeeze(v(:,2,:))';
+        bodyDebug.LFEP = bodyDebug.MIDPEL + bodyDebug.getPelvisLength()/2*v;
+        bodyDebug.RFEP = bodyDebug.MIDPEL - bodyDebug.getPelvisLength()/2*v;
+
+        v = zeros(3, 3, n2);
+        z = (bodyDebug.LFEP-bodyDebug.LFEO)';
+        z = z ./ vecnorm(z, 2, 1);
+        v(:, 3, :) = reshape(z, 3, 1, []);
+        y =  quat2rotm(bodyDebug.qLSK);
+        v(:, 2, :) = y(:, 2, :);
+        x = cross(v(:, 2, :), v(:, 3, :));
+        x = x ./ vecnorm(x, 2, 1);
+        v(:, 1, :) =  reshape(x, 3, 1, []);
+        bodyDebug.qLTH = rotm2quat(v);
+
+        v = zeros(3, 3, n2);
+        z = (bodyDebug.RFEP-bodyDebug.RFEO)';
+        z = z ./ vecnorm(z, 2, 1);
+        v(:, 3, :) = reshape(z, 3, 1, []);
+        y =  quat2rotm(bodyDebug.qRSK);
+        v(:, 2, :) = y(:, 2, :);
+        x = cross(v(:, 2, :), v(:, 3, :));
+        x = x ./ vecnorm(x, 2, 1);
+        v(:, 1, :) =  reshape(x, 3, 1, []);
+        bodyDebug.qRTH = rotm2quat(v);
+        
+        if strcmp(algo, 'lieekfv1')
+            idx = {{'PELV', 1:3}, {'LANK', 4:6}, {'RANK', 7:9}};
+            for i=1:3
+                sname = sprintf('%sVel%s', idx{i}{1}, suffix);
+                sensorsDebug.(sname)(1:3:n2, :) = debugData.xhatPri.vec(idx{i}{2},:)';
+                sensorsDebug.(sname)(2:3:n2, :) = debugData.xhatPos.vec(idx{i}{2},:)';
+                sensorsDebug.(sname)(3:3:n2, :) = debugData.xtilde.vec(idx{i}{2},:)';
+            end
+        else
+            idx = {{'PELV',  4: 6}, {'LANK', 14:16}, {'RANK', 24:26}};
+            for i=1:3
+                sname = sprintf('%sVel%s', idx{i}{1}, suffix);
+                sensorsDebug.(sname)(1:3:n2, :) = debugData.predState(:,idx{i}{2});
+                sensorsDebug.(sname)(2:3:n2, :) = debugData.zuptState(:,idx{i}{2});
+                sensorsDebug.(sname)(3:3:n2, :) = debugData.cstrState(:,idx{i}{2});
+            end
+        end
+    end
+    bodyDebug.nSamples = bodyDebug.nSamples*3;
+    bodyDebug.fs = bodyDebug.fs*3;
+end
