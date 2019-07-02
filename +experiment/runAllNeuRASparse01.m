9a832d33e5390d271e3c70f233f316002a5edb08
lsy3 <l.sy@unsw.edu.au>
2019-07-02 11:36:56 +1000

    fixed parameters

--- +experiment/runAllNeuRASparse01.m
+++ +experiment/runAllNeuRASparse01.m
@@ -96,7 +96,7 @@ for nsI = 1:length(nsList)
 
     dataN = size(dataList, 1);
 
-    for i = [1 15]
+    for i = [15]
         n = table2struct(dataList(i, :));
         
         uwbDistSigma = 0.0;
