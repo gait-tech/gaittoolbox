e6d6b9f5088a524d4723c417f6f47502dcdf9f08
Luke Sy <sylukewicent@gmail.com>
2019-10-15 18:42:59 +1100

    rebuilt docs

--- +mocapdb/@BVHBody/getEndIndex.m
+++
@@ -1,9 +0,0 @@
-function endIdx = getEndIndex(obj)
-	% Get end index
-	%
-	% :return: endIdx - last index
-	%
-	% .. Author: - Luke Sy (UNSW GSBME)
-
-    endIdx = length(obj.Hips(:,1));
-end
--- +mocapdb/@ViconBody/getEndIndex.m
+++
@@ -1,8 +0,0 @@
-function endIdx = getEndIndex(obj)
-	% Get end index
-	%
-	% :return: endIdx - last index
-	%
-	% .. Author: - Luke Sy (UNSW GSBME)
-    endIdx = length(obj.PELV(:,1));
-end
